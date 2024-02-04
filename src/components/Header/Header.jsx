2902fef8727dd594b1a68404ba0e27c0c67014ff
OlehKhv <hvostik.oleg@ukr.net>
2024-02-04 22:10:06 +0200

    add logo to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,24 @@
-import { HeaderContainer, Navigation, StyledNavLink } from "./Header.styled";
+import {
+    HeaderContainer,
+    IconWrapper,
+    Navigation,
+    Span,
+    StyledNavLink,
+} from "./Header.styled";
+import sprite from "../../assets/images/sprite.svg";
 
 export const Header = () => {
     return (
         <HeaderContainer>
             <Navigation>
-                <StyledNavLink to="/">Home</StyledNavLink>
+                <StyledNavLink to="/">
+                    <Span>
+                        <IconWrapper>
+                            <use href={`${sprite}#icon-logo`} />
+                        </IconWrapper>
+                        Home
+                    </Span>
+                </StyledNavLink>
                 <StyledNavLink to="catalog">Catalog</StyledNavLink>
                 <StyledNavLink to="favorites">Favorites</StyledNavLink>
             </Navigation>
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -7,7 +7,7 @@ export const HeaderContainer = styled.header`
 
 export const Navigation = styled.nav`
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
 `;
 
@@ -15,7 +15,7 @@ export const StyledNavLink = styled(NavLink)`
     padding: 8px 16px;
     color: #ffffff;
     font-weight: 600;
-    font-size: 24px;
+    font-size: 32px;
     line-height: 1.42857;
     text-shadow:
         0 0 5px #3470ff,
@@ -38,4 +38,17 @@ export const StyledNavLink = styled(NavLink)`
             0 0 40px #3470ff,
             0 0 80px #3470ff;
     }
+`;
+
+export const IconWrapper = styled.svg`
+    margin-right: 12px;
+    width: 32px;
+    height: 32px;
+    fill: #3470ff;
+    display: inline-block;
+`;
+
+export const Span = styled.span`
+    display: flex;
+    align-items: center;
 `;
